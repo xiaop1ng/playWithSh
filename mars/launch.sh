858c5538a3ac4fe438310a30633caecaa1723ae8
xiaop1ng <jianchaoping@gmail.com>
2022-04-24 11:42:51 +0800

    Update launch.sh
    update

--- mars/launch.sh
+++ mars/launch.sh
@@ -6,8 +6,17 @@ cd ~
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 sudo tar xvzf ngrok-v3-stable-linux-amd64.tgz -C /usr/local/bin
 ngrok config add-authtoken 28D5wL6ch4tVxNZOYmcM18GzXgq_wrg6a8Sj1RLwSYC1krhy
-ngrok tcp 1984
 
-printf "done\n";
-printf "{$SSPASSWORD}\n";
-printf "aes-256-cfb\n";
+while true
+do
+    if netstat -tunlp | grep 1984 | grep LISTEN | read line
+    then
+        echo "shadowsocks is up!"
+        sleep 3s
+        ngrok tcp 1984
+        break
+    fi
+	echo "wait for shadowsocks up, 5 seconds"
+	# sleep for 5 seconds
+	sleep 5s
+done
