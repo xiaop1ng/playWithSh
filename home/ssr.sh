9601d1e684018edcd217133aaaac8000665d1c92
xiaop1ng <jianchaoping@gmail.com>
2024-01-11 00:34:35 +0800

    更新 ssr.sh

--- home/ssr.sh
+++ home/ssr.sh
@@ -13,7 +13,7 @@ export TIMESTAMP=`date +%s`
 # create file: frp clien config
 cat>~/frpc.ini<<EOF
 [common]
-server_addr = 14.154.133.13
+server_addr = 14.153.151.142
 server_port = 7000
 [tcp$TIMESTAMP]
 type = tcp
